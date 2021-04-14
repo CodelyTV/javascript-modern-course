8775109106b2d1f5909d5aa27fe3357b04293436
Nuria <soriano.nuria@gmail.com>
2021-04-14 10:49:18 +0200

    add lesson 6-2: node list start

---
+++ 62-node-list/js/utils.js
@@ -0,0 +1,35 @@
+export function countWords(str) {
+  return str.split(" ").length;
+}
+
+export function countChars(str) {
+  return str.split("").length;
+}
+
+export function iterateNodes(array, callback) {
+  for (let i = 0; i < array.length; ++i) {
+    callback(array[i]);
+  }
+}
+
+export function show(element) {
+  element.classList.remove("hidden");
+}
+
+export function hide(element) {
+  element.classList.add("hidden");
+}
+
+export function createUser(form, callback) {
+  callback({
+    success: true,
+    data: {
+      firstName: form.elements.firstName.value,
+      lastName: form.elements.lastName.value,
+      email: form.elements.email.value,
+      dob: form.elements.dob.value,
+      country: form.elements.country.value,
+      bio: form.elements.bio.value,
+    },
+  });
+}
