b8113b4073555e9cef11a107eabb1b3c21c0ba4f
ismanapa <ismanapa@gmail.com>
2021-04-20 19:03:41 +0200

    add lesson 8-1: promises start

---
+++ 81-async/js/utils.js
@@ -0,0 +1,29 @@
+export function countWords(str) {
+  return str.split(" ").length;
+}
+
+export function countChars(str) {
+  return str.split("").length;
+}
+
+export function show(element) {
+  element.classList.remove("hidden");
+}
+
+export function hide(element) {
+  element.classList.add("hidden");
+}
+
+export function createUser(form, callback) {
+  const newUser = Object.values(form.elements).reduce((user, element) => {
+    if (element.id) {
+      user[element.id] = element.value;
+    }
+    return user;
+  }, {});
+
+  callback({
+    success: true,
+    data: newUser,
+  });
+}
